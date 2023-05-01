04e0021765970b70ef8f8f6d7f6311827a60cc2c
Kaiden Parcher <ktparcher@.com>
2023-05-01 09:22:03 -0700

    Add in table for employees

--- db/schema.sql
+++ db/schema.sql
@@ -12,3 +12,16 @@ CREATE TABLE roles (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(500) NOT NULL
 );
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(500) NOT NULL,
+    last_name VARCHAR(500) NOT NULL,
+    dept_id INT,
+    role_id INT,
+    manager_id INT,
+    salary INT NOT NULL,
+    FOREIGN KEY (dept_id) REFERENCES departments(id) ON DELETE SET NULL, 
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL   
+);
