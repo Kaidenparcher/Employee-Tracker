20ad5c2c68b47896dc90a0170d02ac1cc9cbaa96
Kaiden Parcher <ktparcher@.com>
2023-05-01 21:40:25 -0700

    attempt new ideas

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ function start() {
 // Function to view all roles
 function viewRoles() {
     connection.query(
-      "SELECT roles.id, roles.name, departments.name AS department FROM roles LEFT JOIN departments ON roles.dept_id = departments.id",
+      "SELECT * FROM roles",
       (err, res) => {
         if (err) throw err;
         console.table(res);
@@ -89,7 +89,7 @@ function viewRoles() {
   }
   
   // Function to view all employees
-function viewEmployees() {
+  function viewEmployees() {
     connection.query(
       "SELECT employees.id, employees.first_name, employees.last_name, roles.name AS title, departments.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employees LEFT JOIN roles ON employees.role_id = roles.id LEFT JOIN departments ON roles.dept_id = departments.id LEFT JOIN employees manager ON employees.manager_id = manager.id",
       (err, res) => {
